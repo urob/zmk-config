962f36b4fd6ff8172cbae83abce0d7c339427113
lu-kan <dexterken@gmail.com>
2023-04-18 18:04:47 +0800

    36 42

--- config/extra_keys.h
+++ config/extra_keys.h
@@ -1,4 +1,4 @@
-// The default layout has 34 keys. Additional keys can be added by pre-setting any of
+// The default layout has 36 keys. Additional keys can be added by pre-setting any of
 // the macros defined in this file to one or more keys before sourcing this file.
 
 /* left of left half */
@@ -11,24 +11,6 @@
 #if !defined X_LB  // bottom row, left
     #define X_LB
 #endif
-#if !defined X_LH  // thumb row, left
-    #define X_LH
-#endif
-
-/* between left and right half 
-#if !defined X_MT  // top row, middle
-    #define X_MT
-#endif
-#if !defined X_MM  // middle row, middle
-    #define X_MM
-#endif
-#if !defined X_MB  // bottom row, middle
-    #define X_MB
-#endif
-#if !defined X_MH  // thumb row, middle
-    #define X_MH
-#endif
-*/
 
 /* right of right half */
 #if !defined X_RT  // top row, right
@@ -39,8 +21,5 @@
 #endif
 #if !defined X_RB  // bottom row, right
     #define X_RB
-#endif
-#if !defined X_RH  // thumb row, right
-    #define X_RH
 #endif
 
