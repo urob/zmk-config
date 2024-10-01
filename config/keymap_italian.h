d9d740047d369e56701d6ac66a6239d14132696f
Lemonade4400 <6310070+Lemonade4400@users.noreply.github.com>
2024-10-01 21:36:19 +0200

    include italian_keymap.h

---
+++ config/keymap_italian.h
@@ -0,0 +1,122 @@
+/**
+ * This file includes custom keycodes for the italian input language setting.
+ * If you only type in english, you can delete this file and it's import.
+ * If you wish to replace this file with your preferred language, use
+ * the qmk version as reference:
+ * https://github.com/qmk/qmk_firmware/tree/master/quantum/keymap_extras
+*/
+
+
+/*
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ \ │   │   │   │   │   │   │   │   │   │   │ ' │ ì │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │ è │ + │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │ ò │ à │ ù │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ < │   │   │   │   │   │   │   │ , │ . │ - │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define IT_BSLH GRAVE            // backslash
+#define IT_SQUOT MINUS           // '
+#define IT_IGRV EQUAL            // ì
+// Row 2
+#define IT_EGRV LBKT             // è
+#define IT_PLUS SLASH            // +
+// Row 3
+#define IT_OGRV SEMI             // ò
+#define IT_AGRV APOS             // à
+#define IT_UGRV NON_US_HASH      // ù
+// Row 4
+#define IT_LESS NON_US_BSLH      // <
+#define IT_MINUS RBKT            // -
+
+/* Shifted symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ | │ ! │ " │ £ │ $ │ % │ & │ / │ ( │ ) │ = │ ? │ ^ │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │ é │ * │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │ ç │ ° │ § │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define IT_PIPE LS(GRAVE)        // |
+#define IT_EXCL LS(N1)           // !
+#define IT_DQUOT LS(N2)          // "
+#define IT_PND LS(N3)            // £
+#define IT_USD LS(N4)            // $
+#define IT_PERC LS(N5)           // %
+#define IT_AND LS(N6)            // &
+#define IT_FSLH LS(N7)           // /
+#define IT_LPAR LS(N8)           // (
+#define IT_RPAR LS(N9)           // )
+#define IT_EQL LS(N0)            // =
+#define IT_QUES LS(MINUS)        // ?
+#define IT_CARET LS(EQUAL)       // ^
+// Row 2
+#define IT_EACU LS(LBKT)         // é
+#define IT_AST LS(RBKT)          // *
+// Row 3
+#define IT_CCED LS(SEMI)         // ç
+#define IT_DEG LS(APOS)          // °
+#define IT_SECT LS(NON_US_HASH)  // §
+// Row 4
+#define IT_GREAT LS(NON_US_BSLH) // >
+#define IT_UNDER LS(SLASH)       // _
+
+/* AltGr symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │   │   │   │   │   │   │   │   │   │   │   │   │   │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │ € │   │   │   │   │   │   │   │ [ │ ] │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │ @ │ # │   │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │   │   │   │   │   │   │   │   │   │   │   │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 2
+#define IT_EUR RA(E)             // €
+#define IT_LBKT RA(LBKT)         // [
+#define IT_RBKT RA(RBKT)         // ]
+// Row 3
+#define IT_AT RA(SEMI)           // @
+#define IT_HASH RA(APOS)         // #
+
+/* Shift+AltGr symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │   │   │   │   │   │   │   │   │   │   │   │   │   │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │ { │ } │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │   │   │   │   │   │   │   │   │   │   │   │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 2
+#define IT_LBRC LS(RA(LBKT))     // {
+#define IT_RBRC LS(RA(RBKT))     // }
+
+/** 
+ * Personalized symbols.
+ * 
+ * These symbols are not part of the default italian input.
+ * I use a custom windows keymap to input them.
+*/
+#define IT_BKTK RA(MINUS)        // `
+#define IT_TILDE RA(EQUAL)       // ~
+#define IT_CEGR RA(LS(E))        // È
