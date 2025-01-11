baaa607e68a5caa8556ab342b3d9954c51731ca4
urob <978080+urob@users.noreply.github.com>
2025-01-11 16:50:17 -0500

    Bump keymap-drawer and build env

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # Version of requirements.txt installed in pythonEnv
     zephyr.url = "github:zmkfirmware/zephyr/v3.5.0+zmk-fixes";
@@ -19,7 +19,7 @@
     devShells = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       zephyr = zephyr-nix.packages.${system};
-      keymap_drawer = pkgs.python3Packages.callPackage ./draw { };
+      keymap_drawer = pkgs.python3Packages.callPackage ./nix/keymap-drawer.nix { };
 
     in {
       default = pkgs.mkShellNoCC {
@@ -32,21 +32,24 @@
           pkgs.cmake
           pkgs.dtc
           pkgs.ninja
-          # pkgs.ccache
-          # pkgs.dfu-util
-          # pkgs.qemu
 
-          # Uncomment these if you don't have system-wide versions:
+          pkgs.just
+          pkgs.yq # make sure we use the python version
+
+          # -- Uncomment these if you don't have system-wide versions --
           # pkgs.gawk             # awk
           # pkgs.unixtools.column # column
           # pkgs.coreutils        # cp, cut, echo, mkdir, sort, tail, tee, uniq, wc
           # pkgs.diffutils        # diff
           # pkgs.findutils        # find, xargs
           # pkgs.gnugrep          # grep
-          pkgs.just               # just
           # pkgs.gnused           # sed
-          pkgs.yq                 # yq
         ];
+
+        shellHook = ''
+          export ZMK_BUILD_DIR=$(pwd)/.build;
+          export ZMK_SRC_DIR=$(pwd)/zmk/app;
+        '';
       };
     });
   };
