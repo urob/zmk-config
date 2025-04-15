7d439c7e6d9306e80991a766ef7cb7849cad308b
David Lee <dav@davlee.com>
2025-04-15 23:51:23 +1000

    fix: BT profiles are zero indexed. feat: add mod-taps for outer keys; misc tweaks & adds; feat: add SYS layer

--- config/eyelash_matrix.h
+++ config/eyelash_matrix.h
@@ -78,4 +78,6 @@
 #define JSR 21 // joystick right
 #define JSD 35 // joystick down
 
-// .....
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4
+#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2 
