b0d994f46f038adb85487f50142340509a41fdd5
okdmb <48458512+okdmb@users.noreply.github.com>
2025-08-05 17:33:08 +1000

    Update layers.h

--- config/layers.h
+++ config/layers.h
@@ -2,7 +2,19 @@
 
 #define DEF 0
 #define GAM 1
-// 2
+#define NUM 2
+#define NAV 3
+#define FN  4
+#define SYS 5
+#define SWAP 6
+#define L10 7
+#define BUTTON 8
+#define MOUSE 9
+
+/*
+#define DEF 0
+#define GAM 1
+#define
 #define BUTTON 3
 #define NAV 4
 #define MOUSE 5
@@ -11,4 +23,4 @@
 #define FN  8
 #define SYS 9
 #define L10 10
-
+*/
