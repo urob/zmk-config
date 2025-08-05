fd71f6057cd2e817c5c8e7e9364f84ed13f6427b
okdmb <48458512+okdmb@users.noreply.github.com>
2025-08-05 16:56:28 +1000

    BASE & BUTTON layers

--- config/layers.h
+++ config/layers.h
@@ -2,12 +2,13 @@
 
 #define DEF 0
 #define GAM 1
-#define NUM 2
-#define NAV 3
-#define FN  4
-#define SYS 5
+// 2
+#define BUTTON 3
+#define NAV 4
+#define MOUSE 5
 #define SWAP 6
-#define L10 7
-#define BUTTON 8
-#define MOUSE 9
+#define NUM 7
+#define FN  8
+#define SYS 9
+#define L10 10
 
