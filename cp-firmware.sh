7bbc22c5143e51ccea0554851889bcc0b7d5c073
bernelius <bob@bernelius.com>
2025-12-19 15:20:07 +0100

    new enter/escape

--- cp-firmware.sh
+++ cp-firmware.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [[ -d "/Volumes/XIAO-SENSE" ]]; then
-    cp "./firmware/totem_left-seeeduino_xiao_ble.uf2" "/Volumes/XIAO-SENSE/"
+if [[ -d "/run/media/bernelius/XIAO-SENSE" ]]; then
+    cp "./firmware/totem_left-seeeduino_xiao_ble.uf2" "/run/media/bernelius/XIAO-SENSE/"
+    echo "cp command completed."
 else
     echo "ERROR: No XIAO-SENSE found."
 fi
