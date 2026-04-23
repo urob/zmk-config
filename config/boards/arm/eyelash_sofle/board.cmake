bae2d4573c0a972f6b42a1c22c4a3a30cfcba4e8
bernelius <bob@bernelius.com>
2026-04-23 22:10:55 +0200

    complete rewrite of eyelash sofle config to fit the new zephyr api. screen now broken

--- config/boards/arm/eyelash_sofle/board.cmake
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2021 @MangoIV
-# SPDX-License-Identifier: MIT
-
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
